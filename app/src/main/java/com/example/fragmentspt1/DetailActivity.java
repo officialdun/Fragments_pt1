8dcaa61b8ce9e2734d6cb9083761e761559290c9
dark <dncomgt@gmail.com>
2023-07-22 15:54:48 -0400

    first

--- app/src/main/java/com/example/fragmentspt1/DetailActivity.java
+++ app/src/main/java/com/example/fragmentspt1/DetailActivity.java
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
-    public static String EXTRA_WORKOUT_ID;
+    public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        //int id =(int) getIntent().getExtras().get("EXTRA_WORKOUT_ID");
-        int id = 3;
+        int id =(int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+
 
         WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
          frag.setWorkout(id);
--- app/src/main/java/com/example/fragmentspt1/MainActivity.java
+++ app/src/main/java/com/example/fragmentspt1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener{
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     public void itemClicked(long id){
 
-
+//        Toast.makeText(this, String.valueOf(id), Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
         startActivity(intent);
